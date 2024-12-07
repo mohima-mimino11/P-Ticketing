d487af84857baca356ab5a31051fdad4e488ec23
mohima-mimino11 <mohimahoque22@gmail.com>
2024-12-08 01:37:47 +0600

    Js section scrollintoview done and seat selection with background color change added

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,26 @@
 // scrolltosection button
 const scrollToTicketSectionBtn = getTheElementById('btn-to-ticket-section');
-scrollToTicketSectionBtn.addEventListener('click', fu)
+scrollToTicketSectionBtn.addEventListener('click', function(){
+  const seatSelectionSection = getTheElementById('seat-booking-section');
+  seatSelectionSection.scrollIntoView({ behavior: "smooth" });
+})
+
+const seatsInSeatsGrid = document.getElementsByClassName('seats');
+console.log(seatsInSeatsGrid);
+for(let seat of seatsInSeatsGrid){
+  seat.addEventListener('click', function(){
+    seatsId = seat.innerText;
+    console.log(seatsId);
+    addBackgroundColorById(seatsId)
+    removeBackgroundColorById(seatsId)
+    
+  })
+  
+}
+
+
+// seatsInSeatsGrid.addEventListener('click', function(){
+//   console.log(seatsInSeatsGrid);
+  
+// })
+
--- scripts/utility.js
+++ scripts/utility.js
@@ -3,3 +3,13 @@ function getTheElementById(elementId){
    // const elementText = element.innerText;
    return element;
 }
+
+function addBackgroundColorById(elementId){
+   const element = document.getElementById(elementId);
+   element.classList.add('bg-[#1DD100]');
+}
+
+function removeBackgroundColorById(elementId){
+   const element = document.getElementById(elementId);
+   element.classList.remove('bg-[#F7F8F8]');
+}
