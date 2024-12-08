90103ea5c120526d2cc96466775c9d2fa9b925d1
mohima-mimino11 <mohimahoque22@gmail.com>
2024-12-08 20:21:02 +0600

    Js functionality of avaialable seats number, selected seat number, coupon code button enabling, form button enabling, total and grand total price update done

--- scripts/main.js
+++ scripts/main.js
@@ -6,21 +6,96 @@ scrollToTicketSectionBtn.addEventListener('click', function(){
 })
 
 const seatsInSeatsGrid = document.getElementsByClassName('seats');
-console.log(seatsInSeatsGrid);
+// console.log(seatsInSeatsGrid);
 for(let seat of seatsInSeatsGrid){
   seat.addEventListener('click', function(){
     seatsId = seat.innerText;
-    console.log(seatsId);
-    addBackgroundColorById(seatsId)
-    removeBackgroundColorById(seatsId)
+    // console.log(seatsId);
+    // add background color and remove default background color in selected seats
+    addBackgroundColorById(seatsId);
+    removeBackgroundColorById(seatsId);
+    // If a seat is selected and phone number is given only then the “Next” button on form submission will be enabled otherwise the button will be disabled by default
+    const phoneElement = getTheElementById('phone-number');
+    // console.log(phoneElement.value);
+    if(phoneElement.value !== ''){
+      removeAttributeById('seat-confirm-btn');
+    }
+    
+     
+    // show hown many seats are selected and how many are left after user selects some seats
+    // available seats will decrease by 1
+    let seatsLeft = getElementTextById('seats-left');
+    seatsLeft--;
+    // available seats innertext will be updated
+    setElementTextById('seats-left', seatsLeft)
+    // seats number of selected seats will increase by 1
+    let seatsNumber = getElementTextById('seats-number');
+    seatsNumber++;
+    // seat nummber will be updated
+    setElementTextById('seats-number', seatsNumber);
+   
+    
+
+
+
+    //  if 4 seats are selected together by a user and phone number is given then enable the  “Apply” button for coupon code.
+    if(seatsNumber >= 4 && phoneElement.value !== ''){
+      removeAttributeById('coupon-btn');
+      // A user can select at max 4 seats not more than this if a user does that show them a alert message.
+      if(seatsNumber > 4){
+        keepBackgroundColorById(seatsId);
+        setElementTextById('seats-left', 36)
+        setElementTextById('seats-number', 4);
+        alert('You cannot select more than four seats!Please apply your coupon if you have any and click the apply button to verify your coupon.');
+      }
+    }
+    
+    // Append seat, class, and price elements divs
+    appendElementById('append-parent', 'append-item')
+
+    // update total price and grand total
+    const ticketPriceDefault = getElementTextById('price-list');
+    const totalPrice = seatsNumber * ticketPriceDefault;
+    setElementTextById('total-price', totalPrice)
+    const grandTotalPrice = totalPrice;
+    setElementTextById('grand-total-price', grandTotalPrice);
+    const couponApplyBtn = getTheElementById('coupon-btn');
+    // console.log(couponApplyBtn.innerText);
+    couponApplyBtn.addEventListener('click', function(){
+      const couponTextElement = getTheElementById('coupon-text');
+      // console.log(couponTextElement.value);
+      // after giving valid coupon code (New15, Couple 20) the coupon code section should be hidden and discounted price should be considered and grand total should be updated.
+      if(couponTextElement.value === 'NEW15'){
+        const discountedPrice = totalPrice * (15 / 100);
+        let grandTotalPrice = totalPrice - discountedPrice;
+        setElementTextById('grand-total-price', Math.round(grandTotalPrice));
+        hideElementById('hide-after-valid-coupon');
+      }else if(couponTextElement.value === 'Couple20'){
+        const discountedPrice = totalPrice * (20 / 100);
+        let grandTotalPrice = totalPrice - discountedPrice;
+        setElementTextById('grand-total-price', Math.round(grandTotalPrice));
+        hideElementById('hide-after-valid-coupon');
+      }else{
+        // If a user give invalid coupon code show them an alert
+        alert('your coupon is not valid!')
+      }
+    })
+    
+    
+
+    
+    
+    
+    
+    
     
   })
   
 }
 
 
-// seatsInSeatsGrid.addEventListener('click', function(){
-//   console.log(seatsInSeatsGrid);
-  
-// })
+
+
+
+
 
--- scripts/utility.js
+++ scripts/utility.js
@@ -13,3 +13,42 @@ function removeBackgroundColorById(elementId){
    const element = document.getElementById(elementId);
    element.classList.remove('bg-[#F7F8F8]');
 }
+
+function keepBackgroundColorById(elementId){
+   const element = document.getElementById(elementId);
+   element.classList.add('bg-[#F7F8F8]')
+}
+
+function removeAttributeById(elementId){
+   const element = document.getElementById(elementId);
+   element.classList.remove('disabled');
+}
+
+function getElementTextById(elementId){
+   const element = document.getElementById(elementId);
+   const elementText = element.innerText;
+   const value = parseInt(elementText);
+   return value;
+ }
+ 
+ function setElementTextById(elementId, value){
+   const element = document.getElementById(elementId);
+   element.innerText = value;
+ }
+
+function hideElementById(elementId){
+   const element = document.getElementById(elementId);
+   element.classList.add('hidden');
+}
+
+function appendElementById(elementId, appendItemId){
+   const element = document.getElementById(elementId);
+   const appendElement = document.createElement(appendItemId);
+   appendElement.innerHTML = `<div  class="flex justify-center items-center gap-20 border-b border-dashed border-[#33030712]">
+                  <p class="text-gray-500 font-inter text-base flex">C3</p>
+                  <p class="text-gray-500 font-inter text-base">Economy</p>
+                  <p id="price-list" class="text-gray-500 font-inter text-base">550</p>
+                </div>`
+   element.appendChild(appendElement);
+} 
+
